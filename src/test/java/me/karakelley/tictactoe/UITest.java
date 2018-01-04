2875543825ece3f219567762f79f0f791e2a778b
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-04 15:40:13 -0600

    Update test to reflect message vs system.out

--- src/test/java/me/karakelley/tictactoe/UITest.java
+++ src/test/java/me/karakelley/tictactoe/UITest.java
@@ -2,19 +2,14 @@ package me.karakelley.tictactoe;
 
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UITest {
 
     @Test
-    public void welcomeMessage() {
-        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        UI game = new UI();
-        game.welcomeMessage();
-        assertEquals("Welcome to Tic Tac Toe!\n", outContent.toString());
+
+    public void testMessage() {
+        UI userInterface = new UI();
+        assertEquals("Welcome to Tic Tac Toe!", userInterface.message("Welcome to Tic Tac Toe!"));
     }
 }
