656bc817e7d76b6c573ab1bfb6789e1ead69cf78
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-07 20:26:48 -0600

    Fix indentation

--- src/main/java/me/karakelley/tictactoe/Main.java
+++ src/main/java/me/karakelley/tictactoe/Main.java
@@ -3,7 +3,7 @@ package me.karakelley.tictactoe;
 public class Main {
 
     public static void main(String args[]) {
-        UI userInterface = new UI(System.out);
-        userInterface.welcomeMessage();
+      UI userInterface = new UI(System.out);
+      userInterface.welcomeMessage();
     }
 }
