fbd95e579d23e409b1ff6f9539728f90bb169b47
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-08 10:37:54 -0600

    Add function to print board

--- src/main/java/me/karakelley/tictactoe/UI.java
+++ src/main/java/me/karakelley/tictactoe/UI.java
@@ -3,15 +3,21 @@ package me.karakelley.tictactoe;
 import java.io.PrintStream;
 
 public class UI {
-    public PrintStream output;
+  public PrintStream output;
+  private BoardInterface board;
 
-    public UI(PrintStream out) {
-        output = out;
-    }
+  public UI(PrintStream out) {
+    output = out;
+    board = new BoardInterface();
+  }
 
-    public void welcomeMessage() {
-       output.println("Welcome to Tic Tac Toe!");
-    }
+  public void welcomeMessage() {
+    output.println("Welcome to Tic Tac Toe!");
+  }
 
+
+  public void displayBoard() {
+    output.printf(board.setupBoard());
+  }
 }
 
