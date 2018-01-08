f8d8016b61ceec65806c6ebc0ad0564f30b67b44
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-08 13:35:05 -0600

    Add function to get user keyboard input and update class to be initialized with InputStream

--- src/main/java/me/karakelley/tictactoe/UI.java
+++ src/main/java/me/karakelley/tictactoe/UI.java
@@ -1,23 +1,33 @@
 package me.karakelley.tictactoe;
 
+import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class UI {
-  public PrintStream output;
+  private PrintStream out;
+  private Scanner inputScanner;
   private BoardInterface board;
 
-  public UI(PrintStream out) {
-    output = out;
+  public UI(PrintStream out, InputStream in) {
+    this.out = out;
+    inputScanner = new Scanner(in);
     board = new BoardInterface();
   }
 
   public void welcomeMessage() {
-    output.println("Welcome to Tic Tac Toe!");
+    out.println("Welcome to Tic Tac Toe!");
   }
 
 
   public void displayBoard() {
-    output.printf(board.setupBoard());
+    out.printf(board.setupBoard());
+  }
+
+  public void promptEnterKey() {
+    out.println("Please press \"ENTER\" to continue");
+
+    inputScanner.nextLine();
   }
 }
 
