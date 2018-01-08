73414975c676a2d9edb2a10d409bb4bbf470d5cd
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-08 10:38:24 -0600

    Add call to print board

--- src/main/java/me/karakelley/tictactoe/Main.java
+++ src/main/java/me/karakelley/tictactoe/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String args[]) {
       UI userInterface = new UI(System.out);
       userInterface.welcomeMessage();
+      userInterface.displayBoard();
     }
 }
