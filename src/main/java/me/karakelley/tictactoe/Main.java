aafdba92eec5a67fbe26319ce339595d200d25cb
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-08 13:32:45 -0600

    Update UI initialization

--- src/main/java/me/karakelley/tictactoe/Main.java
+++ src/main/java/me/karakelley/tictactoe/Main.java
@@ -3,8 +3,9 @@ package me.karakelley.tictactoe;
 public class Main {
 
     public static void main(String args[]) {
-      UI userInterface = new UI(System.out);
+      UI userInterface = new UI(System.out, System.in);
       userInterface.welcomeMessage();
+      userInterface.promptEnterKey();
       userInterface.displayBoard();
     }
 }
