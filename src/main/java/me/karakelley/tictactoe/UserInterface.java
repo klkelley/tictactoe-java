7b7d466a20abc84f204a36ce22bf70faefd9b621
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-09 14:53:26 -0600

    Change functionality of UserInterface to display information to user

---
+++ src/main/java/me/karakelley/tictactoe/UserInterface.java
@@ -0,0 +1,20 @@
+package me.karakelley.tictactoe;
+
+public class UserInterface {
+  private IO io;
+  private BoardPresenter board;
+
+  public UserInterface(IO io, BoardPresenter board) {
+    this.io = io;
+    this.board = board;
+  }
+
+  public void greetUser() {
+    io.display("Welcome to Tic Tac Toe!\n");
+    io.getInput("Please press \"ENTER\" to continue");
+  }
+
+  public void displayBoard() {
+    io.display(board.setupBoard());
+  }
+}
