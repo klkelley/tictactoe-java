7820b6b2d369add3d91835683fa84ec05780352d
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-09 09:32:14 -0600

    Update to initialize objects and start gamerunner

--- src/main/java/me/karakelley/tictactoe/Main.java
+++ src/main/java/me/karakelley/tictactoe/Main.java
@@ -2,10 +2,12 @@ package me.karakelley.tictactoe;
 
 public class Main {
 
-    public static void main(String args[]) {
-      UI userInterface = new UI(System.out, System.in);
-      userInterface.welcomeMessage();
-      userInterface.promptEnterKey();
-      userInterface.displayBoard();
-    }
+
+  public static void main(String args[]) {
+    UI userInterface = new UI(System.out, System.in);
+    BoardInterface boardInterface = new BoardInterface();
+    GameRunner gameRunner = new GameRunner(userInterface, boardInterface);
+
+    gameRunner.start();
+  }
 }
