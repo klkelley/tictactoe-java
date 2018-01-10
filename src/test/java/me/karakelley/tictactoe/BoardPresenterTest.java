a904c78134220b7c4bf0066fd1b71a3c84009076
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-10 13:45:43 -0600

    Add test for placing marker on board

--- src/test/java/me/karakelley/tictactoe/BoardPresenterTest.java
+++ src/test/java/me/karakelley/tictactoe/BoardPresenterTest.java
@@ -4,23 +4,35 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class BoardPresenterTest {
   private BoardPresenter boardPresenter;
 
+  BoardState boardStateMock = mock(BoardState.class);
+  BoardState boardState = new BoardState(new String[]{"-", "-", "-", "-", "-", "-", "-", "-", "-"});
+
   @BeforeEach
   public void setUp() {
-    boardPresenter = new BoardPresenter();
+    boardPresenter = new BoardPresenter(boardState);
   }
 
   @Test
   public void testSetupBoard() {
-    assertEquals(boardBuilder(), boardPresenter.setupBoard());
+    String[] blankGrid = new String[]{"-","-","-","-","-","-","-","-","-"};
+    assertEquals(testBoardBuilder(blankGrid), boardPresenter.setupBoard());
   }
 
-  private String boardBuilder() {
+  @Test
+  public void testBoardHasOneMarker() {
+    boardState.placeMove(1);
+    String[] testGrid = new String[]{"-","X","-","-","-","-","-","-","-"};
+    assertEquals(testBoardBuilder(testGrid), boardPresenter.setupBoard());
+
+  }
+
+  private String testBoardBuilder(String[] grid) {
     StringBuilder board = new StringBuilder();
-    String[] spaces = new String[]{"0","1","2","3","4","5","6","7","8"};
     String blankSpace = "   |   |   \n";
     String rowDivider = "___________\n";
 
@@ -34,6 +46,18 @@ class BoardPresenterTest {
     }
     board.append("\n ");
 
-    return String.format(board.toString(), spaces);
+    return String.format(board.toString(), boardGridBuilder(grid));
+  }
+
+  private String[] boardGridBuilder(String[] grid) {
+    String[] newGrid = new String[9];
+    for (int n = 0; n < grid.length; n++) {
+      if (grid[n] == "-") {
+        newGrid[n] = Integer.toString(n);
+      } else {
+        newGrid[n] = grid[n];
+      }
+    }
+    return newGrid;
   }
 }
