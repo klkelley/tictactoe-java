eae50931a147484846d0397af9c52cdb78d905d2
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-10 15:08:29 -0600

    Add mocks and update class name

--- src/test/java/me/karakelley/tictactoe/GameTest.java
+++ src/test/java/me/karakelley/tictactoe/GameTest.java
@@ -1,20 +1,28 @@
 package me.karakelley.tictactoe;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
-class GameRunnerTest {
+class GameTest {
+  private Game game;
 
   UserInterface userInterfaceMock = mock(UserInterface.class);
+  HumanPlayer humanPlayerMock = mock(HumanPlayer.class);
+  BoardState boardStateMock = mock(BoardState.class);
+
+  @BeforeEach
+  public void setUp() {
+    game = new Game(userInterfaceMock, humanPlayerMock, boardStateMock);
+  }
+
 
   @Test
   public void testStart() {
-    GameRunner gameRunner = new GameRunner(userInterfaceMock);
-    gameRunner.start();
+    when(humanPlayerMock.nextMove()).thenReturn("1");
+    game.start();
     verify(userInterfaceMock, times(1)).greetUser();
   }
+
 }
