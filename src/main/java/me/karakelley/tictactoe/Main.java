667c1a9435f8f8febc1eab2aa70b36388864c7d6
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-10 13:46:59 -0600

    Add new object initializations

--- src/main/java/me/karakelley/tictactoe/Main.java
+++ src/main/java/me/karakelley/tictactoe/Main.java
@@ -5,7 +5,9 @@ public class Main {
 
   public static void main(String args[]) {
     IO io = new IO(System.out, System.in);
-    BoardPresenter boardPresenter = new BoardPresenter();
+    HumanPlayer humanPlayer = new HumanPlayer(io);
+    BoardState boardState = new BoardState(new String[]{"-", "-", "-", "-", "-", "-", "-", "-", "-"});
+    BoardPresenter boardPresenter = new BoardPresenter(boardState);
     UserInterface userInterface = new UserInterface(io, boardPresenter);
     GameRunner gameRunner = new GameRunner(userInterface);
 
