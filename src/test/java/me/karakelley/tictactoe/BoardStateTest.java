37b24fe4266cde05ce29adccd1c31679bda5d75a
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-10 12:46:27 -0600

    Add first test for placing move on the board

---
+++ src/test/java/me/karakelley/tictactoe/BoardStateTest.java
@@ -0,0 +1,17 @@
+package me.karakelley.tictactoe;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardStateTest {
+
+  @Test
+  public void testPlaceMove() {
+    BoardState boardState = new BoardState(new String[]{"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"});
+    String[] expectedOutput = new String[] {"X","-", "-", "-", "-", "-", "-", "-", "-", "-"};
+    assertArrayEquals(expectedOutput, boardState.placeMove(0));
+  }
+}
