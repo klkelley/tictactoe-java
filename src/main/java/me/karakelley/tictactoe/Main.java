d14c8fe30cdada28729ebc967be707822475d450
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-11 09:40:45 -0600

    Update object initialization and add Validator

--- src/main/java/me/karakelley/tictactoe/Main.java
+++ src/main/java/me/karakelley/tictactoe/Main.java
@@ -5,10 +5,11 @@ public class Main {
 
   public static void main(String args[]) {
     IO io = new IO(System.out, System.in);
-    HumanPlayer humanPlayer = new HumanPlayer(io);
+    HumanPlayer humanPlayer = new HumanPlayer("X");
+    Validator validator = new NumberValidator();
     BoardState boardState = new BoardState(new String[]{"-", "-", "-", "-", "-", "-", "-", "-", "-"});
     BoardPresenter boardPresenter = new BoardPresenter(boardState);
-    UserInterface userInterface = new UserInterface(io, boardPresenter);
+    UserInterface userInterface = new UserInterface(io, boardPresenter, validator);
     Game game = new Game(userInterface, humanPlayer, boardState);
 
     game.start();
