e156b3417aa8938fad2029edd5ea3a70835aaa18
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-11 09:49:41 -0600

    Extract UI initialization to setup method and add test for userPrompt

--- src/test/java/me/karakelley/tictactoe/UserInterfaceTest.java
+++ src/test/java/me/karakelley/tictactoe/UserInterfaceTest.java
@@ -1,19 +1,25 @@
 package me.karakelley.tictactoe;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class UserInterfaceTest {
+  private UserInterface userInterface;
 
   IO ioMock = mock(IO.class);
   BoardPresenter boardPresenterMock = mock(BoardPresenter.class);
   Validator validatorMock = mock(Validator.class);
 
+  @BeforeEach
+  public void setUp() {
+    userInterface = new UserInterface(ioMock, boardPresenterMock, validatorMock);
+  }
+
   @Test
   public void testGreetUser() {
-    UserInterface userInterface = new UserInterface(ioMock, boardPresenterMock, validatorMock);
     doNothing().when(ioMock).display("test");
     userInterface.greetUser();
     verify(ioMock, times(1)).display("Welcome to Tic Tac Toe!\n");
@@ -21,9 +27,16 @@ class UserInterfaceTest {
 
   @Test
   public void testDisplayBoard() {
-    UserInterface userInterface = new UserInterface(ioMock, boardPresenterMock, validatorMock);
     doNothing().when(ioMock).display("test");
     userInterface.displayBoard();
     verify(ioMock, times(1)).display(null);
+  }
+
+  @Test
+  public void testUserPrompt() {
+    when(ioMock.getInput()).thenReturn("1");
+    when(validatorMock.isValidInput("1")).thenReturn(true);
+    doNothing().when(ioMock).display("test");
+    assertEquals("1", userInterface.userPrompt());
   }
 }
