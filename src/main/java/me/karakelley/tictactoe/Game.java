17db21bac4b33d3a974579d09279428920769559
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-12 11:21:39 -0600

    Inject validatorfactory into game class

--- src/main/java/me/karakelley/tictactoe/Game.java
+++ src/main/java/me/karakelley/tictactoe/Game.java
@@ -5,23 +5,25 @@ public class Game {
   private UserInterface userInterface;
   private HumanPlayer humanPlayer;
   private BoardState boardState;
+  private ValidatorFactory validator;
 
-  public Game(UserInterface userInterface, HumanPlayer humanPlayer, BoardState boardState) {
+  public Game(UserInterface userInterface, HumanPlayer humanPlayer, BoardState boardState, ValidatorFactory validator) {
     this.userInterface = userInterface;
     this.humanPlayer = humanPlayer;
     this.boardState = boardState;
+    this.validator = validator;
   }
 
   public void start() {
     userInterface.greetUser();
-    userInterface.userPrompt("Please press \"ENTER\" to continue\n", new EnterValidator());
+    userInterface.userPrompt("Please press \"ENTER\" to continue\n", validator.make("EnterValidator"));
     userInterface.displayBoard(boardState.getGrid());
 
     play();
   }
 
   private void play() {
-    String chosenCell = userInterface.userPrompt("Please enter an available cell:\n", new NumberValidator());
+    String chosenCell = userInterface.userPrompt("Please enter an available cell:\n", validator.make("NumberValidator"));
     boardState.placeMove(chosenCell, humanPlayer.getMarker());
     userInterface.displayBoard(boardState.getGrid());
   }
