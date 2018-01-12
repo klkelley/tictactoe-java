a7a0c6d61416f83be90b86bc79368f684318686b
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-12 11:04:05 -0600

    Add tests for ValidatorFactoryImplementation

---
+++ src/test/java/me/karakelley/tictactoe/ValidatorFactoryImplementationTest.java
@@ -0,0 +1,27 @@
+package me.karakelley.tictactoe;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorFactoryImplementationTest {
+  private ValidatorFactory validator;
+
+  @BeforeEach
+  public void setUp() {
+    validator = new ValidatorFactoryImplementation();
+  }
+
+  @Test
+  public void testMakingNumberValidator() {
+    assertTrue(validator.make("NumberValidator") instanceof NumberValidator);
+  }
+
+  @Test
+  public void testMakingEnterValidator() {
+    assertTrue(validator.make("EnterValidator") instanceof EnterValidator);
+  }
+
+}
