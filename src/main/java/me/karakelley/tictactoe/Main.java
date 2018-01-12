c9b673bd799717636b82ce2909755398d5b5a818
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-12 11:22:02 -0600

    Update changes to reflect new validator factory

--- src/main/java/me/karakelley/tictactoe/Main.java
+++ src/main/java/me/karakelley/tictactoe/Main.java
@@ -6,11 +6,11 @@ public class Main {
   public static void main(String args[]) {
     IO io = new IO(System.out, System.in);
     HumanPlayer humanPlayer = new HumanPlayer("X");
-    Validator validator = new NumberValidator();
+    ValidatorFactory validator = new ValidatorFactoryImplementation();
     BoardState boardState = new BoardState();
     BoardPresenter boardPresenter = new BoardPresenter();
-    UserInterface userInterface = new UserInterface(io, boardPresenter, validator);
-    Game game = new Game(userInterface, humanPlayer, boardState);
+    UserInterface userInterface = new UserInterface(io, boardPresenter);
+    Game game = new Game(userInterface, humanPlayer, boardState, validator);
 
     game.start();
   }
