3f5243ef89a69bfabb439d8f4532ea212a2b492d
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-14 14:15:09 -0600

    Update Main to include new classes

--- src/main/java/me/karakelley/tictactoe/Main.java
+++ src/main/java/me/karakelley/tictactoe/Main.java
@@ -6,13 +6,13 @@ public class Main {
   public static void main(String args[]) {
     IO io = new IO(System.out, System.in);
     HumanPlayer humanPlayer = new HumanPlayer("X");
-    ValidatorFactory validator = new ValidatorFactoryImplementation();
     HumanPlayer humanPlayer1 = new HumanPlayer("O");
+    Game game = new Game();
     BoardState boardState = new BoardState();
     BoardPresenter boardPresenter = new BoardPresenter();
     UserInterface userInterface = new UserInterface(io, boardPresenter);
-    Game game = new Game(userInterface, humanPlayer, humanPlayer1, boardState, validator);
+    GameLoop gameLoop = new GameLoop(userInterface, humanPlayer, humanPlayer1);
 
-    game.start();
+    gameLoop.start(boardState, game);
   }
 }
