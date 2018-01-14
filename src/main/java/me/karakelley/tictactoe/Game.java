c4165aa699c995e4504aa40a4cc20b18a5d318fb
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-14 12:27:20 -0600

    Add game class to check for tie and associated tests

---
+++ src/main/java/me/karakelley/tictactoe/Game.java
@@ -0,0 +1,35 @@
+package me.karakelley.tictactoe;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Game {
+
+  public boolean gameOver(BoardState boardState) {
+    return tie(boardState) || winner(boardState);
+  }
+
+  public boolean tie(BoardState boardState) {
+    return boardState.fullBoard() && !winner(boardState);
+  }
+
+  private boolean winner(BoardState boardState) {
+    boolean winner = false;
+    String pattern = "[A-Z]";
+    String[][] combos = boardState.combinations();
+    for (int i = 0; i < combos.length; i++) {
+      List<String> section = removeDuplicates(combos[i]);
+      if (section.size() == 1 && section.get(0).matches(pattern)) {
+        winner = true;
+      }
+    }
+    return winner;
+  }
+
+  private List<String> removeDuplicates(String[] combinations){
+    List<String> combos = Arrays.asList(combinations);
+    return combos.stream().distinct().collect(Collectors.toList());
+  }
+}
---
+++ src/test/java/me/karakelley/tictactoe/GameTest.java
@@ -0,0 +1,47 @@
+package me.karakelley.tictactoe;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+  private Game game;
+  BoardState boardState = new BoardState();
+
+  @BeforeEach
+  public void setUp() {
+    game = new Game();
+  }
+
+  @Test
+  public void testTie() {
+    boardState.placeMove("0", "O");
+    boardState.placeMove("1", "O");
+    boardState.placeMove("2", "X");
+    boardState.placeMove("3", "X");
+    boardState.placeMove("4", "X");
+    boardState.placeMove("5", "O");
+    boardState.placeMove("6", "O");
+    boardState.placeMove("7", "X");
+    boardState.placeMove("8", "O");
+
+    assertEquals(true, game.tie(boardState));
+  }
+
+  @Test
+  public void testGameOver() {
+    boardState.placeMove("0", "O");
+    boardState.placeMove("1", "O");
+    boardState.placeMove("2", "O");
+    boardState.placeMove("3", "X");
+    boardState.placeMove("4", "X");
+    boardState.placeMove("5", "O");
+    boardState.placeMove("6", "O");
+    boardState.placeMove("7", "X");
+    boardState.placeMove("8", "O");
+
+    assertEquals(true, game.gameOver(boardState));
+  }
+}
