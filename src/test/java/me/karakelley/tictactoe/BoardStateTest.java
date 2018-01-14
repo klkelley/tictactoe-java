3d4220c506c0093ac85b18991707e5136c5f51f7
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-14 12:29:38 -0600

    Add new tests to for getting board combinations and cell availability

--- src/test/java/me/karakelley/tictactoe/BoardStateTest.java
+++ src/test/java/me/karakelley/tictactoe/BoardStateTest.java
@@ -8,18 +8,57 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardStateTest {
+  BoardState boardState;
+
+  @BeforeEach
+  public void setUp() {
+    boardState = new BoardState();
+  }
 
   @Test
   public void testPlaceMove() {
-    BoardState boardState = new BoardState();
     String[] expectedOutput = new String[] {"X","-", "-", "-", "-", "-", "-", "-", "-"};
     assertArrayEquals(expectedOutput, boardState.placeMove("0", "X"));
   }
 
   @Test
   public void testBoardFull() {
-    BoardState boardState = new BoardState(new String[]{"X", "O", "X", "O", "X", "O", "X", "O", "X"});
-
+    boardState.placeMove("0", "X");
+    boardState.placeMove("1", "X");
+    boardState.placeMove("2", "X");
+    boardState.placeMove("3", "X");
+    boardState.placeMove("4", "X");
+    boardState.placeMove("5", "X");
+    boardState.placeMove("6", "X");
+    boardState.placeMove("7", "X");
+    boardState.placeMove("8", "X");
     assertEquals(true, boardState.fullBoard());
+  }
+
+  @Test
+  public void cellAvailable() {
+    assertEquals(true, boardState.cellInBounds(0));
+  }
+
+  @Test
+  public void cellHasMarker() {
+    assertEquals(true, boardState.cellAvailable(0));
+  }
+
+  @Test
+  public void testCombinations() {
+    String[][] expectedOutput = new String[][]{ {"X","X", "X"},
+                                                { "-", "-", "-"},
+                                                { "-", "-", "-"},
+                                                { "X", "-", "-"},
+                                                { "X", "-", "-"},
+                                                { "X", "-", "-"},
+                                                { "X", "-", "-"},
+                                                { "X", "-", "-"}};
+
+    boardState.placeMove("0", "X");
+    boardState.placeMove("1", "X");
+    boardState.placeMove("2", "X");
+    assertArrayEquals(expectedOutput, boardState.combinations());
   }
 }
