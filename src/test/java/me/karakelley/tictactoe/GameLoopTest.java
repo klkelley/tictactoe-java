c4d61e1802dbe437903d785f5a5df8dc8bbca4e2
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-14 14:14:50 -0600

    Remove unused validators

--- src/test/java/me/karakelley/tictactoe/GameLoopTest.java
+++ src/test/java/me/karakelley/tictactoe/GameLoopTest.java
@@ -11,8 +11,6 @@ class GameLoopTest {
   UserInterface userInterfaceMock = mock(UserInterface.class);
   HumanPlayer humanPlayerMock = mock(HumanPlayer.class);
   BoardState boardStateMock = mock(BoardState.class);
-  ValidatorFactory validatorMock = mock(ValidatorFactory.class);
-  NumberValidator numberValidatorMock = mock(NumberValidator.class);
   EnterValidator enterValidatorMock = mock(EnterValidator.class);
   Game gameMock = mock(Game.class);
 
