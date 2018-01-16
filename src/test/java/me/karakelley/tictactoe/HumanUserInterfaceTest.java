e062fbc8e9cbcc3e233d590bb65c42d87d76f518
Kara Kelley <kelley.15@gmail.com>
2018-01-16 14:12:53 -0600

    Add tests for functions that display messages for human game

--- src/test/java/me/karakelley/tictactoe/HumanUserInterfaceTest.java
+++ src/test/java/me/karakelley/tictactoe/HumanUserInterfaceTest.java
@@ -8,8 +8,8 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class UserInterfaceTest {
-  private UserInterface userInterface;
+class HumanUserInterfaceTest {
+  private HumanUserInterface humanUserInterface;
 
   IO ioMock = mock(IO.class);
   BoardPresenter boardPresenterMock = mock(BoardPresenter.class);
@@ -18,20 +18,20 @@ class UserInterfaceTest {
 
   @BeforeEach
   public void setUp() {
-    userInterface = new UserInterface(ioMock, boardPresenterMock);
+    humanUserInterface = new HumanUserInterface(ioMock, boardPresenterMock);
   }
 
   @Test
   public void testDisplayMessage() {
     doNothing().when(ioMock).display("test");
-    userInterface.displayMessage("Welcome to Tic Tac Toe!\n");
+    humanUserInterface.displayMessage("Welcome to Tic Tac Toe!\n");
     verify(ioMock, times(1)).display("Welcome to Tic Tac Toe!\n");
   }
 
   @Test
   public void testDisplayBoard() {
     doNothing().when(ioMock).display("test");
-    userInterface.displayBoard(boardStateMock);
+    humanUserInterface.displayBoard(boardStateMock);
     verify(ioMock, times(1)).display(null);
   }
 
@@ -40,13 +40,42 @@ class UserInterfaceTest {
     when(ioMock.getInput()).thenReturn("1");
     when(numberValidatorMock.isValidInput("1")).thenReturn(true);
     doNothing().when(ioMock).display("test");
-    assertEquals("1", userInterface.userPrompt("test", numberValidatorMock));
+    assertEquals("1", humanUserInterface.userPrompt("test", numberValidatorMock));
   }
 
   @Test
   public void testWaitForKeyPress() throws IOException, InterruptedException {
     when(ioMock.anyKey()).thenReturn(88);
-    userInterface.waitForKeyPress();
+    humanUserInterface.waitForKeyPress();
     verify(ioMock, times(1)).anyKey();
   }
+
+  @Test
+  public void testDisplayTie() {
+    doNothing().when(ioMock).display("It's a tie!\n");
+    humanUserInterface.displayTie();
+    verify(ioMock, times(1)).display("It's a tie!\n");
+  }
+
+  @Test
+  public void testGreetUser() {
+    doNothing().when(ioMock).display("Welcome to Tic Tac Toe!\n");
+    humanUserInterface.greetUser();
+    verify(ioMock, times(1)).display("Welcome to Tic Tac Toe!\n");
+  }
+
+  @Test
+  public void testDisplayWinnerForPlayerOne() {
+    doNothing().when(ioMock).display("Player one wins!\n");
+    humanUserInterface.displayWinner(true);
+    verify(ioMock, times(1)).display("Player one wins!\n");
+  }
+
+  @Test
+  public void testDisplayWinnerForPlayerTwo() {
+    doNothing().when(ioMock).display("Player two wins!\n");
+    humanUserInterface.displayWinner(false);
+    verify(ioMock, times(1)).display("Player two wins!\n");
+  }
+
 }
