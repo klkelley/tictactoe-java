8158884770c7b95a41a83bf050340036a940ff22
Kara Kelley <kelley.15@gmail.com>
2018-01-16 14:12:12 -0600

    Add userinterface interface

---
+++ src/main/java/me/karakelley/tictactoe/UserInterface.java
@@ -0,0 +1,12 @@
+package me.karakelley.tictactoe;
+
+public interface UserInterface {
+
+  void greetUser();
+  void displayWinner(boolean firstPlayerWins);
+  String userPrompt(String message, Validator validator);
+  void waitForKeyPress();
+  void displayBoard(BoardState board);
+  void displayMessage(String message);
+  void displayTie();
+}
