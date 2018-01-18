ff2b63a3f0b64b38cd375dc0d3629cd5500a0cd8
Kara Kelley <kelley.15@gmail.com>
2018-01-18 14:36:57 -0600

    Add GameConfiguration class initialization

--- src/main/java/me/karakelley/tictactoe/Main.java
+++ src/main/java/me/karakelley/tictactoe/Main.java
@@ -1,18 +1,23 @@
 package me.karakelley.tictactoe;
 
+import org.json.simple.parser.ParseException;
+import java.io.IOException;
+
 public class Main {
 
+  public static void main(String[] args) throws IOException, ParseException {
 
-  public static void main(String args[]) {
     IO io = new IO(System.out, System.in);
-    Player humanPlayer = new HumanPlayer("X");
-    Player humanPlayer1 = new HumanPlayer("O");
     Game game = new Game();
     BoardState boardState = new BoardState();
     BoardPresenter boardPresenter = new BoardPresenter();
     UserInterface humanUserInterface = new HumanUserInterface(io, boardPresenter);
-    GameLoop gameLoop = new GameLoop(humanUserInterface, humanPlayer, humanPlayer1);
+    GameConfiguration gameConfiguration = new GameConfiguration();
+    Player humanPlayer = new HumanPlayer(gameConfiguration.getPlayer1marker());
+    Player humanPlayer1 = new HumanPlayer(gameConfiguration.getPlayer2marker());
 
+
+    GameLoop gameLoop = new GameLoop(humanUserInterface, humanPlayer, humanPlayer1);
     gameLoop.start(boardState, game);
   }
 }
