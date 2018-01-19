7a1a979beba5ab6200e5eb4b4fa5e3c09184b17e
Kara Kelley <kelley.15@gmail.com>
2018-01-19 10:42:44 -0600

    Remove exceptions from method signature

--- src/test/java/me/karakelley/tictactoe/GameConfigurationTest.java
+++ src/test/java/me/karakelley/tictactoe/GameConfigurationTest.java
@@ -1,12 +1,6 @@
 package me.karakelley.tictactoe;
 
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.io.FileReader;
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,28 +9,28 @@ class GameConfigurationTest {
   private GameConfiguration gameConfiguration;
 
   @Test
-  public void testGetPlayer1MarkerWithConfig() throws IOException, ParseException {
+  public void testGetPlayer1MarkerWithConfig() {
     System.setProperty("configuration", "/Users/karakelley/IdeaProjects/tictactoe/src/test/resources/config.json");
     gameConfiguration = new GameConfiguration();
     assertEquals("Q", gameConfiguration.getPlayer1marker());
   }
 
   @Test
-  public void testGetPlayer2MarkerWithConfig() throws IOException, ParseException {
+  public void testGetPlayer2MarkerWithConfig() {
     System.setProperty("configuration", "/Users/karakelley/IdeaProjects/tictactoe/src/test/resources/config.json");
     gameConfiguration = new GameConfiguration();
     assertEquals("Z", gameConfiguration.getPlayer2marker());
   }
 
   @Test
-  public void testGetPlayer1MarkerNoConfig() throws IOException, ParseException {
+  public void testGetPlayer1MarkerNoConfig() {
     System.clearProperty("configuration");
     gameConfiguration = new GameConfiguration();
     assertEquals("X", gameConfiguration.getPlayer1marker());
   }
 
   @Test
-  public void testGetPlayer2MarkerNoConfig() throws IOException, ParseException {
+  public void testGetPlayer2MarkerNoConfig() {
     System.clearProperty("configuration");
     gameConfiguration = new GameConfiguration();
     assertEquals("O", gameConfiguration.getPlayer2marker());
