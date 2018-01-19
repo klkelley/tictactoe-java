2687cb799100fd4db37721b76d5733e74efc80ff
Kara Kelley <kelley.15@gmail.com>
2018-01-19 10:45:24 -0600

    Remove exceptions from method signatures

--- src/test/java/me/karakelley/tictactoe/ParserTest.java
+++ src/test/java/me/karakelley/tictactoe/ParserTest.java
@@ -5,10 +5,8 @@ import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -26,18 +24,18 @@ class ParserTest {
   }
 
   @Test
-  public void getMarkers() throws IOException, ParseException {
+  public void getMarkers() {
     Collection<String> expectedOutput = new ArrayList<>(Arrays.asList("Z", "Q"));
     assertEquals(expectedOutput, parser.markers());
   }
 
   @Test
-  public void testFirstMaker() throws IOException, ParseException {
+  public void testFirstMaker() {
     assertEquals("Q", parser.firstMarker());
   }
 
   @Test
-  public void testSecondMarker() throws IOException, ParseException {
+  public void testSecondMarker() {
     assertEquals("Z", parser.secondMarker());
   }
 
