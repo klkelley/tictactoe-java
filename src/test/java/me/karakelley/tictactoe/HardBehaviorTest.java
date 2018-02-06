2a2fbb7504facbbd3f8ad01479ad217f3eaa2554
Kara Kelley <kelley.15@gmail.com>
2018-02-06 14:11:11 -0600

    Add tests for 100% coverage

--- src/test/java/me/karakelley/tictactoe/HardBehaviorTest.java
+++ src/test/java/me/karakelley/tictactoe/HardBehaviorTest.java
@@ -97,4 +97,18 @@ class HardBehaviorTest {
     int cell = Integer.parseInt(player.pickMove(boardState));
     assertTrue(cell > 0);
   }
+
+  @Test
+  public void noAvailableCellsToBlock() {
+    boardState.placeMove("0", "X");
+    boardState.placeMove("1", "O");
+    boardState.placeMove("2", "X");
+    boardState.placeMove("3", "O");
+    boardState.placeMove("4", "X");
+    boardState.placeMove("5", "X");
+    boardState.placeMove("6", "O");
+    boardState.placeMove("7", "X");
+    boardState.placeMove("8", "O");
+    assertTrue(player.pickMove(boardState) == null);
+  }
 }
