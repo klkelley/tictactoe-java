edfd0fb715cca9b2bdacd474cdc6aeda2e7d1216
Kara Kelley <kelley.15@gmail.com>
2018-02-07 14:01:15 -0600

    Remove magic strings

--- src/main/java/me/karakelley/tictactoe/GameInterfaceImplementation.java
+++ src/main/java/me/karakelley/tictactoe/GameInterfaceImplementation.java
@@ -4,6 +4,9 @@ public class GameInterfaceImplementation implements GameInterfaceFactory {
 
   private BoardPresenter boardPresenter;
   private IO io;
+  private final String HUMAN_GAME = "1";
+  private final String EASY_COMPUTER = "2";
+  private final String HARD_COMPUTER = "3";
 
   public GameInterfaceImplementation(IO io, BoardPresenter boardPresenter) {
     this.io = io;
@@ -12,9 +15,9 @@ public class GameInterfaceImplementation implements GameInterfaceFactory {
 
   @Override
   public UserInterface makeGameInterface(String gameType, UserInterface userInterface) {
-    if (gameType.equals("1")) {
+    if (gameType.equals(HUMAN_GAME)) {
       return userInterface;
-    } else if (gameType.equals("2") || gameType.equals("3")) {
+    } else if (gameType.equals(EASY_COMPUTER) || gameType.equals(HARD_COMPUTER)) {
       return new HumanUserInterface(io, boardPresenter, "You win!\n", "You lose!\n");
     } else {
       return new HumanUserInterface(io, boardPresenter, "You never had a chance!\n", "You never had a chance!\n");
